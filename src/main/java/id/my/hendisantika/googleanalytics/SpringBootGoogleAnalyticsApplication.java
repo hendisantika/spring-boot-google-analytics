c5a57352df5921ef8efad745e1bd40a729464a97
hendisantika <hendisantika@yahoo.co.id>
2025-08-31 14:49:14 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/googleanalytics/SpringBootGoogleAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.googleanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGoogleAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGoogleAnalyticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/googleanalytics/SpringBootGoogleAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.googleanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGoogleAnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
